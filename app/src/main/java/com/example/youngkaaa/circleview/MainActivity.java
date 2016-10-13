7b5b9a6c6e6524671d852ed68cc714676be036a5
youngkaaa <youngkaaa@gmail.com>
2016-10-13 13:39:13 +0800

    rotation

--- app/src/main/java/com/example/youngkaaa/circleview/MainActivity.java
+++ app/src/main/java/com/example/youngkaaa/circleview/MainActivity.java
@@ -1,16 +1,32 @@
 package com.example.youngkaaa.circleview;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import com.example.youngkaaa.ycircleview.CircleView;
 
 public class MainActivity extends AppCompatActivity {
     private CircleView circleView;
+    private ImageView mImageView1;
+    private ImageView mImageView2;
+    private Bitmap mBitmap1;
+    private Bitmap mBitmap2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.main2);
         setContentView(R.layout.activity_main);
-
+//        mImageView1= (ImageView) findViewById(R.id.imageView1);
+//        mImageView2= (ImageView) findViewById(R.id.imageView2);
+//        mBitmap1= BitmapFactory.decodeResource(getResources(),R.drawable.jay_jay1);
+//        mImageView1.setImageBitmap(mBitmap1);
+//        Matrix matrix=new Matrix();
+//        matrix.setRotate(120,mImageView1.getWidth()/2,mImageView1.getHeight()/2);
+//        mBitmap2=Bitmap.createBitmap(mBitmap1.getWidth(),mBitmap1.getHeight(), Bitmap.Config.ARGB_8888);
+//        Canvas canvas=new Canvas(mBitmap2);
+//        canvas.drawBitmap(mBitmap1,0,0,new Paint());
+//        mImageView2.setImageBitmap(mBitmap2);
     }
 }
--- circleview/src/main/java/com/example/youngkaaa/ycircleview/CircleView.java
+++ circleview/src/main/java/com/example/youngkaaa/ycircleview/CircleView.java
@@ -11,6 +11,7 @@ import android.graphics.Paint;
 import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -31,6 +32,11 @@ public class CircleView extends View {
     private float leftPadding;
     private float topPadding;
     private Bitmap bitmapPad;
+    private int rotation;
+    private Canvas mCanvas;
+    private Bitmap bitmapCanvas;
+    private int lastX;
+    private int lastY;
 
     public CircleView(Context context) {
         super(context);
@@ -56,10 +62,14 @@ public class CircleView extends View {
         imgSrc = typedArray.getResourceId(R.styleable.CircleView_imgSrc, -1);
         leftPadding = typedArray.getDimension(R.styleable.CircleView_leftPadding, -1);
         topPadding = typedArray.getDimension(R.styleable.CircleView_topPadding, -1);
-
+        rotation = typedArray.getInt(R.styleable.CircleView_rotation, -1);
         Log.d("kklog", "img =====>" + imgSrc);
         Log.d("kklog", "leftPadding =====>" + leftPadding);
         Log.d("kklog", "topPadding =====>" + topPadding);
+        Log.d("kklog", "rotation =====>" + rotation);
+        if(rotation==-1){
+            rotation=0;
+        }
         typedArray.recycle();
     }
 
@@ -71,6 +81,62 @@ public class CircleView extends View {
         viewWidth = Math.min(width, height);    //set the view's height and width were equal forced!!
         setMeasuredDimension(viewWidth, viewWidth);
         radius = viewWidth / 2;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        //避免在onDraw()中new对象而降低效率，所以放在onLayout()中
+        //因为invalidate()方法只会从onDraw()之后往下执行，不会再次执行该方法
+        //而requestLayout()会从头开始执行！
+        if(rotation!=-1&&mCanvas==null){
+            bitmapCanvas=Bitmap.createBitmap(radius*2,radius*2, Bitmap.Config.ARGB_8888);
+            mCanvas=new Canvas(bitmapCanvas);
+        }
+        if (bitmap.getWidth() < bitmap.getHeight() && leftPadding != -1) {
+            throw new IllegalArgumentException("you can't set leftPadding when img's width<height");
+        } else if (bitmap.getWidth() > bitmap.getHeight() && topPadding != -1) {
+            throw new IllegalArgumentException("you can't set topPadding when img's width>height");
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x= (int) event.getX();
+        int y= (int) event.getY();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int deltaX=x-lastX;
+                int deltaY=y-lastY;
+                if(Math.abs(deltaX)>Math.abs(deltaY)){
+                    if(deltaX>0){
+                        Log.d("kklog","CW");
+                    }else if(deltaX<0){
+                        Log.d("kklog","ACW");
+                    }
+                }else{
+                    if(deltaY>0){
+                        Log.d("kklog","CW");
+                    }else if(deltaY<0){
+                        Log.d("kklog","ACW");
+                    }
+                }
+
+
+
+
+                break;
+            case MotionEvent.ACTION_UP:
+
+                break;
+        }
+        lastX=x;
+        lastY=y;
+        return true;
+
     }
 
     private void inits() {
@@ -85,13 +151,8 @@ public class CircleView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
         bitmapWidth = Math.min(bitmap.getWidth(), bitmap.getHeight());
-        if (bitmap.getWidth() < bitmap.getHeight() && leftPadding != -1) {
-            throw new IllegalArgumentException("you can't set leftPadding when img's width<height");
-        } else if (bitmap.getWidth() > bitmap.getHeight() && topPadding != -1) {
-            throw new IllegalArgumentException("you can't set topPadding when img's width>height");
-        }
+
         scaleRatio = viewWidth * 1.0f / bitmapWidth;   //计算图片缩放比例
         if (leftPadding != -1) {
             if (leftPadding + 2 * radius > bitmap.getWidth()) {
@@ -113,8 +174,15 @@ public class CircleView extends View {
         matrix.setScale(scaleRatio, scaleRatio);
         bitmapShader.setLocalMatrix(matrix);
         paint.setShader(bitmapShader);
-        canvas.drawCircle(radius, radius, radius, paint);
+        mCanvas.drawCircle(radius,radius,radius,paint);   //copy circle to bitmapCanvas
+        matrix.reset();
+        matrix.setRotate(rotation,radius,radius);
+        bitmapShader = new BitmapShader(bitmapCanvas, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        bitmapShader.setLocalMatrix(matrix);
+        paint.setShader(bitmapShader);
+        canvas.drawCircle(radius,radius,radius,paint);  //draw finally view
     }
+
 
     private int measureWidth(int widthMeasureSpec) {
         int mode = MeasureSpec.getMode(widthMeasureSpec);
